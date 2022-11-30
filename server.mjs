0f5db4f71c83740d7c071a95ea7e6c53bdb9b5c3
Syedmoiz7 <syedzada7866@gmail.com>
2022-11-30 16:12:04 +0500

    signle page hosting technique

--- server.mjs
+++ server.mjs
@@ -4,14 +4,24 @@ import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000
 
-
-const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, './website/build')));
-
 app.get('/abc', (req, res) => {
     console.log("request ip: ", req.ip);
   res.send('Hello World! ' + new Date().toString());
 })
+
+app.get('/getweather', (req, res) => {
+    console.log("request ip: ", req.ip);
+  res.send('Hello World! ' + new Date().toString());
+})
+
+app.get('/gettime', (req, res) => {
+    console.log("request ip: ", req.ip);
+  res.send('Hello World! ' + new Date().toString());
+})
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './website/build')));
+app.get('*', express.static(path.join(__dirname, './website/build')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
